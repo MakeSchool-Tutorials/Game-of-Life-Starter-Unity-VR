3f3c6e7f904a1ff796ea8ab64db6a75bbbb9fb6b
Zachary <zbarryte@mit.edu>
2016-06-16 09:43:30 -0700

    init

---
+++ GOL/Assets/MyComponents/Grid.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayOrPause() {
+
+	}
+
+	public void Clear() {
+
+	}
+		
+	public void Randomize() {
+
+	}
+
+	public int GetPopulationCount() {
+		return 0;
+	}
+
+	public int GetGenerationCount() {
+		return 0;
+	}
+
+	public void SetEvolutionPeriod(float sliderValue) {
+
+	}
+}
