60ceb5ba135c54a584b97d86239244659ab87bca
Dion Larson <me@dionlarson.com>
2017-06-08 13:54:30 -0400

    Fix line endings

--- GOL/Assets/MyComponents/Cell.cs
+++ GOL/Assets/MyComponents/Cell.cs
@@ -1,16 +1,16 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine
+using System.Collections
 
-public class Cell : MonoBehaviour {
+public class Cell : MonoBehaviour 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	// Use this for initializatio
+	void Start () 
+	
+	
 
-	// Update is called once per frame
-	void Update() {
+	// Update is called once per fram
+	void Update() 
 
-	}
-		
-}
+	
+	
+
--- GOL/Assets/MyComponents/Grid.cs
+++ GOL/Assets/MyComponents/Grid.cs
@@ -1,39 +1,39 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEngine
+using System.Collections
 
-public class Grid : MonoBehaviour {
+public class Grid : MonoBehaviour 
 
-	// Use this for initialization
-	void Start () {
+	// Use this for initializatio
+	void Start () 
 
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-	public void PlayOrPause() {
+	// Update is called once per fram
+	void Update () 
+	
+	
 
-	}
+	public void PlayOrPause() 
 
-	public void Clear() {
+	
 
-	}
-		
-	public void Randomize() {
+	public void Clear() 
 
-	}
+	
+	
+	public void Randomize() 
 
-	public int GetPopulationCount() {
-		return 0;
-	}
+	
 
-	public int GetGenerationCount() {
-		return 0;
-	}
+	public int GetPopulationCount() 
+		return 0
+	
 
-	public void SetEvolutionPeriod(float sliderValue) {
+	public int GetGenerationCount() 
+		return 0
+	
 
-	}
-}
+	public void SetEvolutionPeriod(float sliderValue) 
+
+	
+
